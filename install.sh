703c89328f83d59aa39f7fa2fab01e5bbdf15c88
Josh McArthur <joshua.mcarthur@gmail.com>
2014-09-05 11:10:32 +1200

    Update install script

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ ln -sf $HOME/.dotfiles/ruby/irbrc $HOME/.irbrc
 ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/zsh/zshenv $HOME/.zshenv
 ln -sf $HOME/.dotfiles/zsh/sudojosh.zsh-theme $HOME/.oh-my-zsh/themes/sudojosh.zsh-theme
-ln -sf $HOME/.dotfiles/vim/gvimrc.after $HOME/.gvimrc.after
-ln -sf $HOME/.dotfiles/vim/.vimrc.after $HOME/.vimrc.after
-ln -sf $HOME/.dotfiles/vim/.vimrc.before $HOME/.vimrc.before
-ln -sf $HOME/.dotfiles/vim/plugins $HOME/.janus
+ln -sf $HOME/.dotfiles/vim/gvimrc $HOME/.gvimrc
+ln -sf $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
+ln -sf $HOME/.dotfiles/vim/plugins $HOME/.vim/bundle
