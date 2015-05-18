005bbf9ef9fb2fc207a3826077106317541d11c7
Josh McArthur <joshua.mcarthur@gmail.com>
2015-05-19 08:44:45 +1200

    Fix symlink creation for vim settings

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/zsh/zshenv $HOME/.zshenv
 ln -sf $HOME/.dotfiles/zsh/sudojosh.zsh-theme $HOME/.oh-my-zsh/themes/sudojosh.zsh-theme
 ln -sf $HOME/.dotfiles/vim/gvimrc $HOME/.gvimrc
-ln -sf $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
+ln -sf $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
 ln -sf $HOME/.dotfiles/vim/plugins $HOME/.vim/bundle
