31e89eeedceecec44e9c2e7fb5486bb1b573175b
Josh McArthur <joshua.mcarthur@gmail.com>
2021-03-01 10:53:21 +1300

    Correct ngrok download location

--- bin/install_ngrok.sh
+++ bin/install_ngrok.sh
@@ -1,1 +1,4 @@
-curl -o ngrok.zip https://dl.ngrok.com/darwin_amd64/ngrok.zip && unzip ngrok.zip && rm ngrok.zip && chmod +x ngrok
+curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > ngrok.zip &&\
+ unzip ngrok.zip &&\
+ rm ngrok.zip &&\
+ chmod +x ngrok
